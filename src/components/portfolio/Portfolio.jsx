2101004a195831203214289391f08cc85d8a870b
Michaela Raines <kellar.dev@gmail.com>
2021-09-23 02:22:41 -0400

    updating

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,56 @@
+import { List } from "@material-ui/core"
+import { useEffect, useState } from "react";
+import PortfolioTabs from "../portfolioTabs/PortfolioTabs";
+import "./portfolio.scss"
+import { projects, challenges } from "../../data";
+
+
+export default function Portfolio() {
+
+    const [selected, setSelected] = useState("projects");
+    
+    const [data, setData] = useState([]);
+
+    const list = [
+        { id: "projects", title: "Projects" },
+        { id: "challenges", title: "Coding Challenges" }
+    ];
+
+    useEffect(() => {
+
+        switch (selected){
+            case "projects":
+                setData(projects);
+                break;
+            case "tools":
+                setData(challenges);
+                break;
+            default: 
+                setData(projects);
+        }
+    }, [selected]);
+
+    return (
+        <div className="portfolio" id="portfolio">
+            <h1>Portfolio</h1>
+            <ul>
+                {list.map((item) => 
+                ( <PortfolioTabs 
+                    title = {item.title} 
+                    active = {selected === item.id} 
+                    setSelected = {setSelected}
+                    id = {item.id}
+                    />)
+                )}
+            </ul>
+            <div className="container">
+                    {data.map (d => (
+                        <div className="item">
+                            <img src={d.img} alt="" />
+                            <h3>{d.title}</h3>
+                        </div>
+                    ))}
+            </div>
+        </div>
+    );
+}
